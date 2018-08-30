6a967946bdf348a8369f789959a603fc266feb44
lyvzm <chanleyou@gmail.com>
2018-08-30 14:03:47 +0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,21 @@
+console.log("Up and running!");
+
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+alert(cardsInPlay);
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay [1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.")
+  }
+}
